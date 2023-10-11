0f2e1c190ae7b575b86f5101117bba989682df5d
aspatic <yufengchen8@gmail.com>
2023-10-11 11:40:21 +0800

    Overwrite signal handler for signal 13&15

--- idc/demo/linux_signal_demo.cpp
+++ idc/demo/linux_signal_demo.cpp
@@ -14,8 +14,13 @@ int main(){
 
     //setting system signal handler
     for (int i=1; i<=64; i++){ // Total 64 types of signal in linux
+        // but be adviced SIG=9 cannot be captured in any way
         signal(i, sigHandler);
     }
+    // You can also overwrite previous handler settings
+    signal(15,SIG_IGN);
+    signal(13,SIG_DFL);
+
     while(1){
         printf("执行了一次任务。\n");
         sleep(1);
