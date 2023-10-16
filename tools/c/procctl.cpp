47bc62db346c9e49718a07e7883fa24e7ffd4201
aspatic <yufengchen8@gmail.com>
2023-10-16 14:52:22 +0800

    what happens if exec failed

--- tools/c/procctl.cpp
+++ tools/c/procctl.cpp
@@ -7,7 +7,9 @@ int main (){
 
     //same as:
     //execl("/bin/ls", "sur/bin/ls", "-lt", "/tmp",(char*) 0);
-    int status = execl("/bin/ls", "ls", "-lt", "/tmp/weather/",(char*) 0);
+
+    // 进行一点改动，让这句代码错误无法执行
+    int status = execl("/bin/lstm", "ls", "-lt", "/tmp/weather/",(char*) 0);
     
     // 之后的内容就执行不了了， 因为exec用它参数中指定的程序替换了当前进程的正文段，数据段，堆和栈
     // 之后就去执行别人的程序了
