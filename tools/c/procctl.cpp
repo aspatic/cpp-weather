631e40de3c777477a1270ddf0f748b23c9d7f396
aspatic <yufengchen8@gmail.com>
2023-10-16 15:03:14 +0800

    a real service calling function

--- tools/c/procctl.cpp
+++ tools/c/procctl.cpp
@@ -1,19 +1,24 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
 
 int main (){
-    printf("aaa\n");
 
-    //same as:
-    //execl("/bin/ls", "sur/bin/ls", "-lt", "/tmp",(char*) 0);
-
-    // 进行一点改动，让这句代码错误无法执行
-    int status = execl("/bin/lstm", "ls", "-lt", "/tmp/weather/",(char*) 0);
-    
-    // 之后的内容就执行不了了， 因为exec用它参数中指定的程序替换了当前进程的正文段，数据段，堆和栈
-    // 之后就去执行别人的程序了
-    printf("bbb, status=%d\n",status);//除非exec调用失败了，不然这一行代码是怎么都不会得到执行的
+    // 用fork创建一个子进程，在子进程中调用execl执行新的程序
+    // 只有子进程会被新程序替换，父进程只需要等待子进程结束
+    // 这样实现程序调用的功能
+    while (true){
+        if (fork()==0){
+            int status = execl("/bin/ls", "ls", "-lt", "/tmp/weather/",(char*) 0);  
+        }
+        else if (fork > 0){
+            int status;
+            wait(&status);
+            sleep(5);
+        }
+    }
 
     return 0;
 }
