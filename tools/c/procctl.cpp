c313f6ef01adeeb73e0ebeaab5a4fa54c8534ec3
aspatic <yufengchen8@gmail.com>
2023-10-16 15:28:29 +0800

    A more general and elegant ways of the same thing

--- tools/c/procctl.cpp
+++ tools/c/procctl.cpp
@@ -13,13 +13,23 @@ int main(int argc, char* argv[]){
     // Sample of function calls:
     // ./procctl 5 /bin/ls -lt /tmp/weather/
 
+    char* pargv[argc];
+    for (int i = 2; i < argc; i++){
+        pargv[i-2] = argv[i];
+    }
+    pargv[argc-2] = NULL; // 为什么要这么写呢，做个实验
+
     while (true){
         if (fork()==0){
             // for example:
             //int status = execl("/bin/ls", "ls", "-lt", "/tmp/weather/",(char*) 0);
-            if (argc == 3) execl(argv[2], argv[2], (char*) 0);    
-            if (argc == 4) execl(argv[2], argv[2], argv[3], (char*) 0);
-            if (argc == 5) execl(argv[2], argv[2], argv[3], argv[4],(char*) 0);
+            // if (argc == 3) execl(argv[2], argv[2], (char*) 0);    
+            // if (argc == 4) execl(argv[2], argv[2], argv[3], (char*) 0);
+            // if (argc == 5) execl(argv[2], argv[2], argv[3], argv[4],(char*) 0);
+            
+            // 为了更加优雅地实现这个程序， 我们用另外一种exec程序来实现
+            execv(argv[2], pargv);
+
 
         }
         else if (fork > 0){
