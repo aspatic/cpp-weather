0d6d77bb513f22d5254b8dc64a39d61a9bf0fa0f
aspatic <yufengchen8@gmail.com>
2023-10-16 15:15:34 +0800

    add argc argv[] alignment

--- tools/c/procctl.cpp
+++ tools/c/procctl.cpp
@@ -4,19 +4,22 @@
 #include <sys/types.h>
 #include <sys/wait.h>
 
-int main (){
+int main(int argc, char* argv[]){
 
     // 用fork创建一个子进程，在子进程中调用execl执行新的程序
     // 只有子进程会被新程序替换，父进程只需要等待子进程结束
     // 这样实现程序调用的功能
+
     while (true){
         if (fork()==0){
-            int status = execl("/bin/ls", "ls", "-lt", "/tmp/weather/",(char*) 0);  
+            // for example:
+            //int status = execl("/bin/ls", "ls", "-lt", "/tmp/weather/",(char*) 0);
+            execl(argv[2], argv[2], argv[3], argv[4],(char*) 0);
         }
         else if (fork > 0){
             int status;
             wait(&status);
-            sleep(5);
+            sleep(atoi(argv[1]));
         }
     }
 
