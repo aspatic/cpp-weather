1e79518dcfadc3037b2d1fbfddd3e237d858a82a
aspatic <yufengchen8@gmail.com>
2023-10-27 17:00:23 +0800

    add lock & bug fixes

--- tools/demo/heartbeat.cpp
+++ tools/demo/heartbeat.cpp
@@ -2,6 +2,7 @@
 
 #define MAXNUMP_ 1000   // 假定整个解决方案活动的进程不超过一千个
 #define SHMKEYP_ 0x5095 // 创建共享内存时的key。
+#define SEMKEYP_ 0x5095 // 创建01信号量时的key。
 
 // 进程心跳信息的结构
 struct st_pinfo
@@ -21,8 +22,13 @@ int main (int argc, char* agrv[]){
     // 创建（或获取）共享内存，大小为n * sizeof(struct st_pinfo)
     
     int m_shmid = 0;
-    if ((m_shmid = shmget(MAXNUMP_, SHMKEYP_ * sizeof(struct st_pinfo), 0640|IPC_CREAT)) == -1){
-        printf("shmget(%x) failed\n", MAXNUMP_);
+    if ((m_shmid = shmget(SHMKEYP_, MAXNUMP_ * sizeof(struct st_pinfo), 0640|IPC_CREAT)) == -1){
+        printf("shmget(%x) failed\n", SHMKEYP_);
+        return -1;
+    }
+    CSEM m_sem;
+    if ( (m_sem.init(SEMKEYP_)) == false){
+        printf("m_sem.init(%x) failed\n", SEMKEYP_);
         return -1;
     }
 
@@ -46,12 +52,14 @@ int main (int argc, char* agrv[]){
     
     // 所以当务之急是扫描共享内存中是否有同名ID的残留记录，并尽快覆盖掉它。
     for (int i = 0; i < SHMKEYP_; i++ ){
-        if (m_shm[i].pid = stpinfo.pid()){
+        if (m_shm[i].pid = stpinfo.pid){
             m_pos_avail = i;
             break;
         }
     }
  
+    m_sem.P(); // 加锁 获得共享内存操作权
+
     // 在共享内存查找一个空位置，把当前进程心跳信息存入
     if(m_pos_avail == -1){
         for (int i = 0; i < SHMKEYP_; i++ ){
@@ -65,13 +73,17 @@ int main (int argc, char* agrv[]){
     }
     if(m_pos_avail == -1){
         // 没找到空位置
+        m_sem.V(); // 解锁
         printf("共享内存空间已用完。\n");
         return -1;
     }
     memcpy(m_shm + m_pos_avail, &stpinfo, sizeof(struct st_pinfo));
 
+    m_sem.V(); // 解锁
+
     while(1){
         // “报平安” 更新共享内存中本进程的心跳
+        // 理论上讲这是独享的内存段，不会有竞争问题，应该也就不需要加锁
         m_shm[m_pos_avail].atime = time(0);
 
         sleep(10);
